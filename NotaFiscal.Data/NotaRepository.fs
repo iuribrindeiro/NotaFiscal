dd2e89381d3fa5ae3fb42295c9a0c8cf6fa71bbb
Iuri Brindeiro <iuribrindeiro@gmail.com>
2023-03-17 19:32:23 -0300

    notaRepository

--- NotaFiscal.Data/NotaRepository.fs
+++ NotaFiscal.Data/NotaRepository.fs
@@ -1,6 +1,16 @@
 module NotaFiscal.Data.NotaRepository
 
 open FSharp.Data.Sql
+open NotaFiscal.Domain.NotaFiscalServico
 
 type sql =
     SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, "Server=192.168.0.41,1433; Database=NotaFiscalDb; User Id=iuri; Password=1495;encrypt=False">
+
+let ctx = sql.GetDataContext()
+
+let getNotasAguardandoEnvio =
+    query {
+        for nota in ctx.Dbo.NotaFiscalServico do
+            where (nota.Discriminator = nameof (AguardandoEnvio))
+            select nota
+    }
